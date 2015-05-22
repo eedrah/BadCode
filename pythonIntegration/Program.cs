c1fdcfb90bd98ca87ed8135794c8f20cf7bb3fba
Eedrah <eedrah@users.noreply.github.com>
2015-05-22 16:43:29 +1200

    call python from C#

---
+++ pythonIntegration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pythonIntegration {
+    class Program {
+        static void Main(string[] args)
+        {
+            string python = "print('hello world')";
+
+            string output = new PythonCode().Run(python);
+
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pythonIntegration/PythonCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pythonIntegration {
+    class PythonCode {
+        public string Run(string code) {
+            var process = new Process();
+            process.StartInfo.FileName = @"C:\Program Files (x86)\IronPython 2.7\ipy64.exe";
+            process.StartInfo.Arguments = string.Format("-c \"{0}\"", code);
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+            return output;
+        }
+    }
+}
