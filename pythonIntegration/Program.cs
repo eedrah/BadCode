86b40c3880b4d42629a580d515b768f54e11cc38
Eedrah <eedrah@users.noreply.github.com>
2015-05-23 17:45:15 +1200

    cmd called from node called from python called from c#

--- pythonIntegration/Program.cs
+++ pythonIntegration/Program.cs
@@ -12,7 +12,7 @@ namespace pythonIntegration {
 
             string python = "print('What?!? Python is also strongly typed?!?')" + Environment.NewLine + 
                             "import subprocess " + Environment.NewLine + 
-                            "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\');(function(){return \\'this is ridiculous\\'})()'])" + Environment.NewLine +
+                            "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'echo hi\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
 
             string output = new PythonCode().Run(python);
