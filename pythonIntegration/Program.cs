09305fe29d90d46d2c7da501b2edcf686bc117e3
Eedrah <eedrah@users.noreply.github.com>
2015-05-23 17:52:02 +1200

    ruby called from node called from python called from c#

--- pythonIntegration/Program.cs
+++ pythonIntegration/Program.cs
@@ -12,7 +12,7 @@ namespace pythonIntegration {
 
             string python = "print('What?!? Python is also strongly typed?!?')" + Environment.NewLine + 
                             "import subprocess " + Environment.NewLine + 
-                            "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'echo hi\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
+                            "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
 
             string output = new PythonCode().Run(python);
