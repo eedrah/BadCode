23748bc6b60b051fb84d0a47216c828795d03bdf
Eedrah <eedrah@users.noreply.github.com>
2015-05-25 00:04:01 +1200

    pass test

--- pythonIntegration/BadCode.cs
+++ pythonIntegration/BadCode.cs
@@ -3,14 +3,14 @@
 namespace pythonIntegration {
     public class BadCode {
         public string Run() {
-            Console.WriteLine("C# is so 1999...\n");
+            string cSharp = "C# is so 1999...";
 
             string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine +
                             "import subprocess " + Environment.NewLine +
                             "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'Javascript. What have we done?\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'Once more unto the breach, dear Ruby, once more...\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
 
-            return new PythonCode().Run(python);
+            return cSharp + Environment.NewLine + Environment.NewLine + new PythonCode().Run(python);
         }
     }
 }
