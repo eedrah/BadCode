5342ce8eb543b37122b62c59ea08f4e6146116dd
Eedrah <eedrah@users.noreply.github.com>
2015-05-25 00:30:49 +1200

    unfactor Run method down to one line

--- pythonIntegration/BadCode.cs
+++ pythonIntegration/BadCode.cs
@@ -3,14 +3,7 @@
 namespace pythonIntegration {
     public class BadCode {
         public string Run() {
-            string cSharp = "C# is so 1999...";
-
-            string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine +
-                            "import subprocess " + Environment.NewLine +
-                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", \\\"console.log(\\\\\\\"Javascript. What have we done?\\\\\\\"); var exec = require(\\\\\\\"child_process\\\\\\\").exec;(function(){exec(\\\\\\\"ruby -e \\\\\\\\\\\\\\\"puts \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"Once more unto the breach, dear Ruby, once more...\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\"\\\\\\\", function(err, stdout, stderr){console.log(stdout)}); return \\\\\\\"\\\\\\\";})()\\\"])" + Environment.NewLine +
-                            "print(output)";
-
-            return cSharp + Environment.NewLine + Environment.NewLine + new PythonCode().Run(python);
+            return "C# is so 1999..." + Environment.NewLine + Environment.NewLine + new PythonCode().Run("print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine + "import subprocess " + Environment.NewLine + "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", \\\"console.log(\\\\\\\"Javascript. What have we done?\\\\\\\"); var exec = require(\\\\\\\"child_process\\\\\\\").exec;(function(){exec(\\\\\\\"ruby -e \\\\\\\\\\\\\\\"puts \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"Once more unto the breach, dear Ruby, once more...\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\"\\\\\\\", function(err, stdout, stderr){console.log(stdout)}); return \\\\\\\"\\\\\\\";})()\\\"])" + Environment.NewLine + "print(output)");
         }
     }
 }
