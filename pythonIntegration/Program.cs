db13b2ec28a72bf45f7116e52da7e5b21d078f77
Eedrah <eedrah@users.noreply.github.com>
2015-05-24 21:26:54 +1200

    convert first level single quotes to double

--- pythonIntegration/Program.cs
+++ pythonIntegration/Program.cs
@@ -10,9 +10,9 @@ namespace pythonIntegration {
         {
             Console.WriteLine("I need to break out of this statically typed straight-jacket!!\n");
 
-            string python = "print('What?!? Python is also strongly typed?!?\\n')" + Environment.NewLine + 
+            string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine + 
                             "import subprocess " + Environment.NewLine + 
-                            "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
+                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
 
             string output = new PythonCode().Run(python);
