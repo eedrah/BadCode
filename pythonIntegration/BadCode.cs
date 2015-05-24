0392230d65dd2efaa155a44d99279cd1dfbb3574
Eedrah <eedrah@users.noreply.github.com>
2015-05-24 22:04:45 +1200

    change words

--- pythonIntegration/BadCode.cs
+++ pythonIntegration/BadCode.cs
@@ -3,11 +3,11 @@
 namespace pythonIntegration {
     public class BadCode {
         public string Run() {
-            Console.WriteLine("I need to break out of this statically typed straight-jacket!!\n");
+            Console.WriteLine("C# is so 1999...\n");
 
             string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine +
                             "import subprocess " + Environment.NewLine +
-                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
+                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'Javascript. What have we done?\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'Once more unto the breach, dear Ruby, once more...\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
 
             return new PythonCode().Run(python);
