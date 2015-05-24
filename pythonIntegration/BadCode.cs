93ebf63c678dda86c482ede09eb3da028e29ab39
Eedrah <eedrah@users.noreply.github.com>
2015-05-24 21:34:50 +1200

    refactor console interaction out to separate class

---
+++ pythonIntegration/BadCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pythonIntegration {
+    public class BadCode {
+        public string Run() {
+            Console.WriteLine("I need to break out of this statically typed straight-jacket!!\n");
+
+            string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine +
+                            "import subprocess " + Environment.NewLine +
+                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
+                            "print(output)";
+
+            return new PythonCode().Run(python);
+        }
+    }
+}
--- pythonIntegration/Program.cs
+++ pythonIntegration/Program.cs
@@ -1,24 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pythonIntegration {
-    class Program {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("I need to break out of this statically typed straight-jacket!!\n");
-
-            string python = "print(\\\"What?!? Python is also strongly typed?!?\\n\\\")" + Environment.NewLine + 
-                            "import subprocess " + Environment.NewLine + 
-                            "output = subprocess.check_output([\\\"node\\\", \\\"-p\\\", 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
-                            "print(output)";
-
-            string output = new PythonCode().Run(python);
-
-            Console.WriteLine(output);
-
+    public class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(new BadCode().Run());
             Console.ReadKey();
         }
     }
