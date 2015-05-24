1d82bfc19aa21d9f000a5215924febed0a7890e4
Eedrah <eedrah@users.noreply.github.com>
2015-05-24 21:20:16 +1200

    add lines between each language

--- pythonIntegration/Program.cs
+++ pythonIntegration/Program.cs
@@ -8,9 +8,9 @@ namespace pythonIntegration {
     class Program {
         static void Main(string[] args)
         {
-            Console.WriteLine("I need to break out of this statically typed straight-jacket!!");
+            Console.WriteLine("I need to break out of this statically typed straight-jacket!!\n");
 
-            string python = "print('What?!? Python is also strongly typed?!?')" + Environment.NewLine + 
+            string python = "print('What?!? Python is also strongly typed?!?\\n')" + Environment.NewLine + 
                             "import subprocess " + Environment.NewLine + 
                             "output = subprocess.check_output(['node', '-p', 'console.log(\\'I hate you Javascript\\'); var exec = require(\\'child_process\\').exec;(function(){exec(\\'ruby -e \\\"puts \\\\\\'WHAT?? Can it be?? Ruby!!\\\\\\' \\\"\\', function(err, stdout, stderr){console.log(stdout)}); return \\'\\';})()'])" + Environment.NewLine +
                             "print(output)";
